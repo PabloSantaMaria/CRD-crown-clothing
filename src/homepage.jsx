d172ab4d2821b2f5025e22acc9fbccc12d0485fa
Pablo Santa María <pablosantamaria@pop-os.localdomain>
2020-07-21 17:20:56 -0300

    initial commit, creado con create-react-app

---
+++ src/homepage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './homepage.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+            <div className='menu-item'>
+                <div className='content'>
+                    <h1>ITEM</h1>
+                    <span>SHOP NOW</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
