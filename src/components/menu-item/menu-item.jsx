6fb4f6846278df411c57b89490f8eb9a5cec1f8a
Pablo Santa María <pablosantamaria@pop-os.localdomain>
2020-07-21 23:19:26 -0300

    directory components

---
+++ src/components/menu-item/menu-item.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './menu-item.scss';
+
+const MenuItem = ({title, imageUrl, size}) => (
+    <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+        <div className='content'>
+            <h1>{title}</h1>
+            <span>SHOP NOW</span>
+        </div>
+    </div>
+);
+
+// `url(`+{imageUrl}+`)`
+
+// `url(${imageUrl})`
+export default MenuItem;
