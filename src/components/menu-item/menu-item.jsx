1d6830caa012ad598f6cb94357b6dc463b280a79
Pablo Santa María <pablosantamaria@pop-os.localdomain>
2020-07-22 09:46:27 -0300

    section 4 completa

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './menu-item.scss';
 
 const MenuItem = ({title, imageUrl, size}) => (
-    <div style={{backgroundImage: `url(${imageUrl})`}} className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+        <div
+        className='background-image'
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+        />
         <div className='content'>
-            <h1>{title}</h1>
-            <span>SHOP NOW</span>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
 );
 
-// `url(`+{imageUrl}+`)`
-
-// `url(${imageUrl})`
 export default MenuItem;
