54759eff86efcccfc0fccd4c3c596dfd36c536d1
Pablo Santa María <pablosantamaria@pop-os.localdomain>
2020-07-26 22:53:47 -0300

    6.72

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import SHOP_DATA from './shop-data.js';
+import CollectionPreview from '../../components/collection-preview/collection-preview.jsx';
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        return (<div className='shop-page'>
+                    {this.state.collections.map(
+                        (collection) => (
+                            <CollectionPreview 
+                            key={collection.id} 
+                            title={collection.title} 
+                            items={collection.items} />)
+                    )}
+                </div>)
+            
+    }
+}
+
+export default ShopPage;
