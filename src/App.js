75070d8649e35d870f08e646c4d0937828a22b33
Pablo Santa María <pablosantamaria@pop-os.localdomain>
2020-07-27 23:30:50 -0300

    6.75 header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 
-import './App.css';
+import Header from './components/header/header';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
+
+import './App.css';
 
 function App() {
   return (
     <div>
+    <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage} />
