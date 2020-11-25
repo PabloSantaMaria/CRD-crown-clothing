147627e1234aa65b9ef19772fccfdddb2a46abc4
PabloSantaMaria <pabloj.santamaria@gmail.com>
2020-11-25 00:36:02 -0300

    updated dependencies, directory and shop data to redux. CollectionsOverview component

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,41 +1,9 @@
-import React from 'react';
+import CollectionsOverview from "../../components/collections-overview/collections-overview";
 
-import SHOP_DATA from './shop-data.js';
-import CollectionPreview from '../../components/collection-preview/collection-preview.jsx';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-    
-// render() {
-//     return (
-//         <div className='shop-page'>
-//             {this.state.collections.map(
-//                 (collection) => (
-//                     <CollectionPreview 
-//                     key={collection.id} 
-//                     title={collection.title} 
-//                     items={collection.items} />)
-//             )}
-//         </div>
-//     )     
-// }
-}
+const ShopPage = () => (
+  <div className='shop-page'>
+    <CollectionsOverview/>
+  </div>
+);
 
 export default ShopPage;
